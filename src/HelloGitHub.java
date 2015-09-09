237c9ea7e3888e01bb229baa924d60c7de8b050b
Tamás Fazekas <tamas.fazekas.hu@gmail.com>
2015-09-09 15:35:40 +0200

    klein Änderung vom 2. Entwickler

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -4,7 +4,7 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello GitHub!");
+		System.out.println("Hello GitHub, I'm developer 2!");
 	
 	}
 
